16b3c7395c696e6ca4724b198fa9d84faee629e7
ChandlerSerres <47650442+ChandlerSerres@users.noreply.github.com>
2021-10-30 17:47:06 -0400

    Create CameraManager.cs

---
+++ Space Dracula Vs Space Hitler/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.LookAt(target);
+    }
+
+    private void LateUpdate()
+    {
+        if(target.transform.position.x > -5.52 && target.transform.position.x < 19.53)
+        {
+            this.transform.position = new Vector3(target.transform.position.x, 1.23f, -30);
+        }
+        else if(target.transform.position.x < -5.52)
+        {
+            this.transform.position = new Vector3(-5.52f, 1.23f, -30);
+        }else if(target.transform.position.x > 19.53) this.transform.position = new Vector3(19.532f, 1.23f, -30);
+
+    }
+}
